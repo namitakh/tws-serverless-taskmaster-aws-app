338fd8a82c7833d5af53742d4b286e6b1f1d0854
namitakh <148350726+namitakh@users.noreply.github.com>
2025-01-28 23:32:24 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From namita!",
+        message: "Hello From namita khajone!",
       },
       null,
       2
